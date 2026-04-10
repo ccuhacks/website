b4a88df7d4a8d07131fad91f61042324ddf4da40
Ritam Das <ritam.das3110@outlook.com>
2026-04-10 13:57:26 +0000

    add /rsvp redirect to rsvp form

---
+++ src/routes/rsvp/+page.svelte
@@ -0,0 +1,8 @@
+<section class="bg-raisin flex h-screen w-screen items-center justify-center">
+	<a
+		href="https://ccuhacks.fillout.com/rsvp"
+		class="text-lavender hover:text-snow font-dotgothic text-2xl underline md:text-4xl"
+	>
+		{'>>>'} RSVP to ccuhacks v1 {'<<<'}</a
+	>
+</section>
