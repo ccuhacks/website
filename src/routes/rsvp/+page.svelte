84b12ad80a6d50859ccce90302c6c3150ea83b42
Sadrita Neogi <sadritaneogi@gmail.com>
2026-05-25 20:00:23 +0530

    Register Using Devfolio Button Is Integrated

--- src/routes/rsvp/+page.svelte
+++ src/routes/rsvp/+page.svelte
@@ -1,6 +1,6 @@
 <section class="bg-raisin flex h-screen w-screen items-center justify-center">
 	<a
-		href="https://ccuhacks.fillout.com/rsvp"
+		href="https://ccuhacks.devfolio.co/"
 		class="text-lavender hover:text-snow font-dotgothic text-2xl underline md:text-4xl"
 	>
 		{'>>>'} RSVP to ccuhacks v1 {'<<<'}</a
