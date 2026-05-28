4521d33e39eef32b413365673818a6a75051cfaf
Ritam Das <ritam.das3110@outlook.com>
2026-05-28 13:56:44 +0000

    add email template and plausible

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,3 +1,21 @@
+<svelte:head>
+	<!-- Privacy-friendly analytics by Plausible -->
+	<script async src="https://plausible.io/js/pa-PUkxNwM1-X52X389ZMH9Q.js"></script>
+	<script>
+		((window.plausible =
+			window.plausible ||
+			function () {
+				(plausible.q = plausible.q || []).push(arguments);
+			}),
+			(plausible.init =
+				plausible.init ||
+				function (i) {
+					plausible.o = i || {};
+				}));
+		plausible.init();
+	</script>
+</svelte:head>
+
 <section class="bg-raisin flex h-screen w-screen items-center justify-center">
 	<a
 		href="https://ccuhacks.devfolio.co/"
